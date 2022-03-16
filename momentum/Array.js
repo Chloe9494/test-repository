b45199a6fde6208fde918f7d0396f6f016b43cf0
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-03-16 23:49:41 +0900

    220316_javascript

---
+++ momentum/Array.js
@@ -0,0 +1,11 @@
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+ 
+//배열에서 요소 가져오기
+console.log(daysOfWeek);
+
+// 배열에서 특정 순서에 해당하는 요소 가져오기 
+console.log(daysOfWeek[3]);
+
+//배열에 요소 추가하기 (괄호안의 요소가 추가되어진 배열이 하나 더 만들어진다)
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
---
+++ momentum/app.js
@@ -0,0 +1,21 @@
+// const playerName = "chloe";
+// const playerPoints = "1994";
+// const playerTall = true;
+// const playerFat = "little bit";
+
+const player = {
+    name: "chloe",
+    points: 10,
+    fat: true,
+};
+
+// console.log(player);
+
+// console.log(player.name); 
+// console.log(player["name"]);  
+// 같은 결과값을 반환한다
+
+console.log(player);
+player.lastName ="potato";  //plyer.property
+player.points = 15;
+console.log(player);
