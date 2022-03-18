7565437fa8afa54bd3988f0b5a78f5e2aba40de1
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-03-18 22:19:20 +0900

    220318 javascript

--- momentum/Array.js
+++ momentum/Array.js
@@ -5,7 +5,7 @@ console.log(daysOfWeek);
 
 // 배열에서 특정 순서에 해당하는 요소 가져오기
 // 첫 요소 순서는 번호가 0부터 부여된다 (ex> 0 1 2 3 4 5) 
-console.log(daysOfWeek[3]);
+console.log(daysOfWeek[3]); //이 경우에는 4번째 요소인 thu가 호출되는 것
 
 //배열에 요소 추가하기 (괄호안의 요소가 추가되어진 배열이 하나 더 만들어진다)
 daysOfWeek.push("sun");
