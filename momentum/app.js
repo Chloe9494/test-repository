ee70c5f1c5ac05b6bd56856e043505657683cfb7
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-03-19 22:58:18 +0900

    220319 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -1,23 +1,5 @@
-// const playerName = "chloe";
-// const playerPoints = "1994";
-// const playerTall = true;
-// const playerFat = "little bit";
+function sayHello() {
+    
+}
 
-const player = {
-    name: "chloe",
-    points: 10,
-    fat: true,
-};
-console.log(player);
-player.fat = false;
-//이 경우엔 constant가 수정된 것이 아니라(CUZ constant는 수정될 수 없기 때문)
-//Object 안의 정보를 수정하게 되는 것이다.
-
-// console.log(player.name); 
-// console.log(player["name"]);  
-// 둘은 같은 결과값을 반환한다
-
-console.log(player);
-player.lastName ="potato";  //plyer.property
-player.points = 15;
-console.log(player);
+// function이란 반복해서 사용할 수 있는 코드조각이다.
---
+++ momentum/objects.js
@@ -0,0 +1,28 @@
+// const playerName = "chloe";
+// const playerPoints = "1994";
+// const playerTall = true;
+// const playerFat = "little bit";
+
+const player = {
+    name: "chloe",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+player.fat = false;
+//이 경우엔 constant가 수정된 것이 아니라(CUZ constant는 수정될 수 없기 때문)
+//Object 안의 정보를 수정하게 되는 것이다.
+
+// console.log(player.name); 
+// console.log(player["name"]);  
+// 둘은 같은 결과값을 반환한다
+
+console.log(player);
+player.lastName ="potato";  //plyer.property
+player.points = 15;
+console.log(player);
+
+
+console.log(player);
+player.points = player.points +15;
+console.log(player.po);
