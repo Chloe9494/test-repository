f24d6248b4ab9321a0310fd9007db4f24fc37897
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-03-22 23:06:49 +0900

    220322 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,11 @@
-function sayHello() {
-console.log("Hello!");
+function sayHello(nameOfPerson, age) {
+console.log("Hi my name is " + nameOfPerson + " I'm " + age + " years old");
 }
 // function이란 반복해서 사용할 수 있는 코드조각이다.
 // 실행할 때마다 블록 안에 작성된 코드가 실행되는것 
 
-// sayHello(); 
-//function을 실행하게 만든다
+sayHello("Chloe", 22);
+sayHello("Jason", 18);
+sayHello("Dyke", 42); 
+//sayHello()의 괄호 안을 실행하게 만든다
 
