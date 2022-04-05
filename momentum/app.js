c280981e299f1ba969f352cf3ff9da3c7ef2360e
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-04-05 23:51:18 +0900

    220405 JAVASCRIPT

--- momentum/app.js
+++ momentum/app.js
@@ -1,5 +1,7 @@
-const age = prompt ("how old are you?");
+const age = parseInt (prompt ("how old are you?") );
 
+console.log( isNaN (age));  // age가 NaN이 아니라는 뜻
+// 숫자를 받으면 숫자로 표기하고, 숫자가 아닌 문자열을 받으면 NaN으로 표기한다
 
-console.log(typeof age, typeof parseInt("15") );    // typeof는 받은 값의 타입을 알려준다! parseInt는 string을 number로 바꿔준다!
-console.log(15, parseInt(age));
+// console.log(typeof age, typeof parseInt("15") );    // typeof는 받은 값의 타입을 알려준다! parseInt는 string을 number로 바꿔준다!
+// console.log(15, parseInt(age));
