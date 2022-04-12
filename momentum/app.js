33ff3e052594a34e311ebb1c0cf29eb06c35107f
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-04-12 23:54:45 +0900

    220412 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -6,13 +6,16 @@ const age = parseInt (prompt ("how old are you?") );
 // console.log(typeof age, typeof parseInt("15") );    // typeof는 받은 값의 타입을 알려준다! parseInt는 string을 number로 바꿔준다!
 // console.log(15, parseInt(age));
 
-if(isNaN (age)) {
-    console.log("Please write a number");
+if(isNaN (age) || age < 0) {
+    console.log("Please write a real positive number");
 } else if (age < 18){
     console.log("You are too young");
 }
   else if (age >= 18 && age <= 50){
     console.log("You can drink");
+} 
+  else if (age >= 50 && age <= 80){
+    console.log("You should do exercise");
 }  
    else if (age > 100) {
     console.log("Are you sure?...");
