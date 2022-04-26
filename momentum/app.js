95033f7b38767e518403067af29c93fad1cc3cc4
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-04-26 23:56:21 +0900

    220426 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -1,21 +1,13 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
     title.style.color = "blue";
 }
 
-function handleMouseEnter(){
-    console.log("mouse is here");
+function handleMouseEnter() {
+    title.innerText = "Mouse is here";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseenter", handleMouseEnter);
-
-const title = document.getElementById("something");
-
-title.innenText = "Got you!";
-
-console.log(title.className);
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone";
+}
---
+++ momentum/events.js
@@ -0,0 +1,21 @@
+const title = document.querySelector("div.hello:first-child h1");
+
+console.dir(title);
+
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    console.log("mouse is here");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseenter", handleMouseEnter);
+
+const title = document.getElementById("something");
+
+title.innenText = "Got you!";
+
+console.log(title.className);
