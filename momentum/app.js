54bbde522d808e2718b54b0ea518e590db2f5b57
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-04-30 23:33:40 +0900

    220430 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -1,34 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    title.style.color = "blue";
-}
-
-function handleMouseEnter() {
-    title.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-    title.innerText = "Mouse is gone";
-}
-
-function handleUserLeave() {
-    title.innerText = "user is gone";
-}
-
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
+   const currentColor = h1.style.color;
+   let newColor;
+    if (currentColor === "blue") {
+       h1.style.color = "tomato";
+   } else {
+       h1.style.color = "blue";
+   }
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("online", handleWindowOnline);
