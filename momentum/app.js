fa2b27e7dc0c0864cb709b2a1f56ed44ab7c2098
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-05-02 23:57:02 +0900

    220502 javascript2

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,3 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
-}
-
-h1.addEventListener("click", handleTitleClick);
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
