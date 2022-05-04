badf6d152ad2dd145070956d6be6799123c5baab
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-05-04 23:50:21 +0900

    220504 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,13 @@
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-    console.dir(loginInput.value);
-    console.log("click!!!");
+    const username = loginInput.value;
+    if (username === "") {
+        alert("please write your name");
+    } else if(username.length) {
+        alert("Your name is too long.")
+    }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
