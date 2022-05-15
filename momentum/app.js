d844aae3d3e9dc9009ea61a869873ea59212942c
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-05-15 23:51:41 +0900

    220515 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -1,18 +1,11 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    if (username === "") {
-        alert("please write your name");
-    } else if (username.length> 15) {
-        alert("Your name is too long");
-    }
+function onLoginBtnClick(toamto) {
+    toamto.preventDefault();
+    console.log(tomato);
 }
 
-function handleLinkClick(){
-    alert("clicked!");
-}
 
 loginForm.addEventListener("submit", onLoginSubmit);
 //link.addEventListener("click", handleLinkClick);
