9f86ff10ab542248117d1383271b8753302a4d5f
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-05-20 23:49:01 +0900

    220520 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -8,20 +8,6 @@ function onLoginBtnClick(event) {
     console.log(loginInput.value);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
-}
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
 
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-
-const link = document.querySelector("a");
-
-function onLoginBtnClick(event) {
-    toamto.preventDefault();
-    console.log(loginInput.value);
-}
