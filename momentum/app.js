602b5997cd8e36a2da1ad49f2e3d7f59e438c68a
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-05-31 23:52:10 +0900

    220531 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -6,21 +6,18 @@ const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const saveUsername = localStorage.getItem(USERNAME_KEY);
+const saveUsername = localStorage.getItem("username");
 
 if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
-} else {
-    greeting.innerText = `Hello ${savedUsername}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-}
 
+} else {
+
+}
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
