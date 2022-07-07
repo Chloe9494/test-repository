14aaad90fde821cd69d1536d2a135dd58b9153a6
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-07-07 23:59:51 +0900

    220707 javascript

--- momentum/objects.js
+++ momentum/objects.js
@@ -26,7 +26,3 @@ console.log(player);
 console.log(player);
 player.points = player.points +15;
 console.log(player.po);
-
-console.log(player);
-player.height = player.points +20;
-console.log(player.po);
