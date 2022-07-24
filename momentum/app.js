576d606bcf5e30eec40a14f9fcb542bba0879c57
Chloe Kim <72907557+Chloe9494@users.noreply.github.com>
2022-07-24 23:55:46 +0900

    220724 javascript

--- momentum/app.js
+++ momentum/app.js
@@ -7,10 +7,6 @@ function onLoginSubmitClick(event) {
     console.log(loginInput.value);
 }
 
-function onLoginSubmitClick(event) {
-    event.preventDefault();
-    console.log(loginInput.value);
-}
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
